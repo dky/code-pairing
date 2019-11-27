67ba52b8f596cbce48ae919f3b34d44a090ff992
Don Ky <don.d.ky@gmail.com>
2019-11-27 17:57:33 -0500

    Print outer, inner loop

--- 11-26-19/loop.go
+++ 11-26-19/loop.go
@@ -5,8 +5,9 @@ import "fmt"
 func main() {
 	// for init; condition; post{}
 	for i := 0; i <= 3-1; i++ {
+		fmt.Printf("The outer loop: %d\n", i)
 		for j := 0; j <= 4-1; j++ {
-			fmt.Printf("The outer loop: %d\t The inner loop: %d\n", i, j)
+			fmt.Printf("\t\tThe inner loop: %d\n", j)
 		}
 	}
 }
