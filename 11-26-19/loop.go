b12e96a0832fe4540594e05eceebfca5dc1d4903
Don Ky <don.d.ky@gmail.com>
2019-11-27 17:09:46 -0500

    Update loop demo

--- 11-26-19/loop.go
+++ 11-26-19/loop.go
@@ -5,8 +5,8 @@ import "fmt"
 func main() {
 	// for init; condition; post{}
 	for i := 0; i <= 3-1; i++ {
-		for j := 0; j <= 2-1; j++ {
-			fmt.Println(j)
+		for j := 0; j <= 4-1; j++ {
+			fmt.Printf("The outer loop: %d\t The inner loop: %d\n", i, j)
 		}
 	}
 }
