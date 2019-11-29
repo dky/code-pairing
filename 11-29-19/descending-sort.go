281af83fe0153f92c80255e9205eb5ec9875b7c2
Don Ky <don.d.ky@gmail.com>
2019-11-29 15:57:41 -0500

    Rename incorrect to descending

--- 11-29-19/descending-sort.go
+++ 11-29-19/descending-sort.go
@@ -55,7 +55,7 @@ func order(i, j int, input []int) {
 
 func main() {
 	//unsortedList := []int{30, 10, 20} //3 element list
-	unsortedList := []int{30, 10, 20, 40} //4 element list
-	//unsortedList := []int{50, 40, 20, 30, 10} //5 element list
+	//unsortedList := []int{30, 10, 20, 40} //4 element list
+	unsortedList := []int{50, 40, 20, 30, 10} //5 element list
 	bubbleSort(unsortedList)
 }
