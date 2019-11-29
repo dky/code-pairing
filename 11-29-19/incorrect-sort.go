684c57682a175155374b97454562ca18ca6440d5
Don Ky <don.d.ky@gmail.com>
2019-11-29 14:21:26 -0500

    Break out and format order

--- 11-29-19/incorrect-sort.go
+++ 11-29-19/incorrect-sort.go
@@ -2,33 +2,48 @@ package main
 
 import "fmt"
 
-func main() {
-	//unsorted := []int{30, 20, 10}
-	unsorted := []int{50, 40, 20, 30, 10}
-	bubbleSort(unsorted)
-}
-
-// The issue with incorrectSort is it returns the sort in decending order. Why?
+// bubbleSort takes in a slice of ints
 func bubbleSort(input []int) {
-	fmt.Println("Number of elements in slice:", len(input))
+	fmt.Printf("Number of elements in slice: %d\n", len(input))
+	fmt.Printf("Unsorted slice: %d\n\n", input)
+
 	//Start from index position 1
 	for i := 1; i < len(input); i++ {
 		//Start from index position 0
 		for j := 0; j < len(input); j++ {
-			fmt.Printf("index i: %d, element i: %d index j: %d, element j: %d\n", i, input[i], j, input[j])
-			//order(input[i], input[j])
+			//fmt.Printf("index i: %d, element i: %d index j: %d, element j: %d\n", i, input[i], j, input[j])
+			fmt.Printf("index i: %d, index j: %d\n", i, j)
+			fmt.Printf("element i: %d element j: %d\n", input[i], input[j])
+			order(input[i], input[j], input)
 		}
 	}
-	fmt.Println("Sorted output:", input)
 }
 
-// order takes in the input[i] and input[j] and swaps their place values if they are in the wrong place.
-func order(i, j int) {
+// order takes in the input[i] and input[j] and swaps their place values if i > j
+func order(i, j int, inputSlice []int) []int {
+	fmt.Printf("comparing element i: %d and element j: %d\n", i, j)
 	if i > j {
-		// Create a tmp var to store the value of i
-		tmp := i
-		i = j
-		j = tmp
-		fmt.Printf("Comparing i: %d, j: %d\n", i, j)
+		fmt.Println("i is greater than j, swapping values")
+
+		tmp := i //tmp stores the value of i
+		i = j    //assign j to i
+		j = tmp  //assign tmp to j
+
+		fmt.Printf("i: %d, j: %d\n", i, j)
+		fmt.Println(inputSlice)
+		fmt.Printf("\n")
+	} else {
+		fmt.Println("i is not greater than j, going back to top of loop j")
+		fmt.Println(inputSlice)
+		fmt.Printf("\n")
 	}
+
+	return inputSlice
+}
+
+func main() {
+	unsortedList := []int{30, 10, 20}
+	//unsortedList := []int{30, 10, 20, 40}
+	//unsortedList := []int{50, 40, 20, 30, 10}
+	bubbleSort(unsortedList)
 }
