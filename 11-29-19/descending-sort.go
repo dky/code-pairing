d2b19793cc1e13401196d92b1efaa4e323de5fcb
Don Ky <don.d.ky@gmail.com>
2019-11-29 16:10:36 -0500

    Append elements and unsorted to output

--- 11-29-19/descending-sort.go
+++ 11-29-19/descending-sort.go
@@ -9,15 +9,14 @@ func printSlice(currentSlice []int) {
 
 // bubbleSort takes in a slice of ints
 func bubbleSort(input []int) {
-	fmt.Printf("Number of elements in slice: %d\n", len(input))
-	fmt.Printf("Unsorted slice: %d\n\n", input)
 
 	//Start from index position 1
 	//dky why do we start from 1 vs 0?
 	for i := 1; i < len(input); i++ {
 		//Start from index position 0
 		for j := 0; j < len(input); j++ {
-			//fmt.Printf("index i: %d, element i: %d index j: %d, element j: %d\n", i, input[i], j, input[j])
+			fmt.Printf("Number of elements in slice: %d\n", len(input))
+			fmt.Printf("Unsorted slice: %d\n", input)
 			fmt.Printf("index i: %d, index j: %d\n", i, j)
 			fmt.Printf("element i: %d element j: %d\n", input[i], input[j])
 			order(i, j, input)
@@ -52,8 +51,8 @@ func order(i, j int, input []int) {
 }
 
 func main() {
-	unsortedList := []int{35, 12, 40} //3 element list
+	//unsortedList := []int{35, 12, 40} //3 element list
 	//unsortedList := []int{30, 10, 20, 40} //4 element list
-	//unsortedList := []int{50, 40, 20, 30, 10} //5 element list
+	unsortedList := []int{50, 40, 20, 30, 10} //5 element list
 	bubbleSort(unsortedList)
 }
