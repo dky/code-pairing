298591dbe5c415bd2e8b47bde72c5461f27942cb
Don Ky <don.d.ky@gmail.com>
2019-12-01 16:04:58 -0500

    Start selection sort

---
+++ 12-1-19/selection_sort.go
@@ -0,0 +1,18 @@
+package main
+
+// Adapting this from https://www.youtube.com/watch?v=GUDLRan2DWM
+
+func main() {
+	arrayA := []int{2, 7, 4, 1, 5, 3}
+
+	for _, v := range arrayA {
+		max := 0
+
+		//trying to find out the largest number in the array.
+		//if v < max {
+		//max = v
+		//}
+
+	}
+
+}
