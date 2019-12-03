8146ced000e697a2d2a36ae96f3255cd60920f36
Don Ky <don.d.ky@gmail.com>
2019-12-02 19:16:47 -0500

    Partial selection sort implementation

--- 12-1-19/selection_sort.go
+++ 12-1-19/selection_sort.go
@@ -1,18 +1,37 @@
 package main
 
+import "fmt"
+
 // Adapting this from https://www.youtube.com/watch?v=GUDLRan2DWM
 
 func main() {
-	arrayA := []int{2, 7, 4, 1, 5, 3}
+	arrayA := []int{3, 1, 5, 2, 4}
 
-	for _, v := range arrayA {
-		max := 0
+	min := arrayA[0]
+	sliceLength := len(arrayA)
+	//is 8 < 8? n
+	//is 7 < 8? y min = 7
 
-		//trying to find out the largest number in the array.
-		//if v < max {
-		//max = v
-		//}
+	for i := 1; i < sliceLength; i++ {
+		fmt.Printf("Loop count: %d\n", i)
+		if arrayA[i] < min {
+			fmt.Printf("Is %d < %d?\n", arrayA[i], min)
+			fmt.Printf("yes, min: %d\n", arrayA[i])
+			min = arrayA[i]
 
+			//arrayA[0] = min
+			for j := i + 1; j < sliceLength; j++ {
+
+				fmt.Println(j)
+
+			}
+
+		} else {
+			fmt.Printf("%d > %d\n", arrayA[i], min)
+		}
 	}
+
+	//fmt.Println(min)
+	fmt.Println(arrayA)
 
 }
